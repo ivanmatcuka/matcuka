dcb76999194f1e034068f0ba5cc84080309babcb
Ivan Matcuka <ivanmatcuka@protonmail.com>
2025-07-24 17:14:40 +0200

    refactor: responsive layout

--- packages/client/src/components/Sections/Section.svelte
+++ packages/client/src/components/Sections/Section.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section
-	class="m-auto flex w-full max-w-xs flex-col items-center gap-10 py-8 md:py-10 xl:max-w-5xl"
+	class="m-auto flex w-full max-w-xs flex-col items-center gap-10 px-2 py-8 sm:max-w-5xl md:py-10"
 >
 	{@render children()}
 </section>
