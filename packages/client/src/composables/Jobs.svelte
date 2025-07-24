ae0da2961ca6971e42578b72213fb0dd4beb0489
Ivan Matcuka <ivanmatcuka@protonmail.com>
2025-07-24 16:22:41 +0200

    refactor: whitespace

--- packages/client/src/composables/Jobs.svelte
+++ packages/client/src/composables/Jobs.svelte
@@ -26,7 +26,9 @@
 
 <hr class="border-primary-100 -mt-10 w-screen border-b-2 border-solid" />
 
-<div class="border-primary-100 flex w-full flex-col items-center gap-3 border-2 border-solid p-2">
+<div
+	class="border-primary-100 flex w-full flex-col items-center gap-3 whitespace-pre-line border-2 border-solid p-2"
+>
 	{#if jobs[activeJobIdxex]?.description}
 		<Typography variant="body1" color="neutral-white">
 			{jobs[activeJobIdxex]?.description}
